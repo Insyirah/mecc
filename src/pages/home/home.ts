5ccdb3f486284dec83dc0c54c6a8e636dcf3bf91
insyirah <nurinsyirahsuhaimi@gmai.com>
2017-10-19 16:24:36 +0800

    d

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  avatars : any[];
+  ava:any[];
+constructor(public navCtrl : NavController) {
 
-  constructor(public navCtrl: NavController) {
+this.avatars = [
+  {category:'1',img:"assets/dino.jpg"},
+  {category:'1',img:"assets/dino.jpg"},
+  {category:'1',img:"assets/dino.jpg"},
+  {category:'1',img:"assets/dino.jpg"},
+  {category:'1',img:"assets/dino.jpg"},
+  {category:'1',img:"assets/dino.jpg"},
+];
 
-  }
+
+}
+
+test(){
+  alert("hoi")
+}
 
 }
