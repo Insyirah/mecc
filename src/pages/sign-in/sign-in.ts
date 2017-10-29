e8fd1b1ae4607f6e60af227c1f0fe609acbb6442
syafiq <syafiqabdrh@gmail.com>
2017-10-30 02:53:57 +0800

    www

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams, ModalController} from 'ionic-angular';
-
-import {TabsPage} from '../tabs/tabs';
+import { IonicPage, NavController, NavParams, ModalController, ViewController } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms/";
 
 /**
  * Generated class for the SignInPage page.
@@ -16,38 +16,135 @@ import {TabsPage} from '../tabs/tabs';
   templateUrl: 'sign-in.html',
 })
 export class SignInPage {
-passwordForm : boolean = false;
-usernameForm : boolean = false;
-planCase : any;
-
-constructor(public navCtrl : NavController, public navParams : NavParams,public modalCtrl: ModalController) {
+  FinalForm: any;
+  user: any;
+  no: number;
+  passwordForm: boolean = false;
+  usernameForm: boolean = false;
+  planCase: any;
+  logInForm: FormGroup;
+  submitForm: { email: any; phoneNumber: string; userName: string; password: any; type: number; };
+  ph: boolean;
+  emails: boolean;
+  constructor(private view: ViewController, private fb: FormBuilder, public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
+    this.logInForm = this.fb.group({
+      name: [''],
+      password: ['', Validators.required]
+    });
   }
+
+  //  dismiss() {
+  //    // Returning data from the modal:
+  //    
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignInPage');
     this.planCase = this.navParams.get("planCase");
 
-    
-    switch (this.planCase){
+
+    switch (this.planCase) {
       case "userName":
-      this.usernameForm = true;
-      break
+        this.usernameForm = true;
+        break
 
       case "pw":
-      this.passwordForm = true;
-      break
+        this.passwordForm = true;
+        break
     }
   }
 
-  goPassword(){
-    let myModal = this.modalCtrl.create(SignInPage,{
+  goPassword(x) {
+    let myModal = this.modalCtrl.create(SignInPage, {
+      name: x.name,
       planCase: "pw"
     });
     myModal.present();
+
   }
 
-  goSignIn(){
+  goSignIn(form) {
+    form.name = this.navParams.get("name");
+    this.FinalForm = this.SubmitLogIn(form)
+    console.log(this.FinalForm)
+
     this.navCtrl.setRoot(TabsPage)
+  }
+
+
+
+  SubmitLogIn(form) {
+    this.emails = this.emailFilter(form.name)
+    console.log(this.emails)
+    if (this.emails == true) {
+      this.submitForm = {
+        email: form.name,
+        phoneNumber: "",
+        userName: "",
+        password: form.password,
+        type: 1
+      }
+      console.log(this.submitForm)
+      //  this.goLogInMeccapan(this.submitForm)
+    }
+    else {
+      this.ph = this.phFilter(form.name)
+
+      if (this.ph == true) {
+        this.submitForm = {
+          email: "",
+          phoneNumber: form.name,
+          userName: "",
+          password: form.password,
+          type: 1
+        }
+
+        console.log(this.submitForm)
+        // this.goLogInMeccapan(this.submitForm)
+      }
+
+      else {
+        this.submitForm = {
+          email: "",
+          phoneNumber: "",
+          userName: form.name,
+          password: form.password,
+          type: 1
+        }
+        console.log(this.submitForm)
+        //   this.goLogInMeccapan(this.submitForm)
+      }
+    }
+return this.submitForm
+
+  }
+
+
+
+
+  emailFilter(x): boolean {
+    if (x.indexOf("@") == -1) {
+      return false
+    }
+    else {
+      var res = x.slice(x.indexOf("@") + 1);
+
+      if (res.indexOf("@") == -1 && res.indexOf(".") != -1) {
+        return true
+      }
+      else {
+        return false
+      }
+    }
+  }
+
+  phFilter(x): boolean {
+    this.no = parseInt(x)
+    if (this.no.toString().length == x.length) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 
 }
--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,37 +1,115 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams, ModalController} from 'ionic-angular';
-import {RegisterPage} from '../register/register';
-import {HomePage} from '../home/home';
-import {TabsPage} from '../tabs/tabs';
-import {SignInPage} from '../sign-in/sign-in';
+import { IonicPage, NavController, NavParams, ModalController, Platform } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+import { HomePage } from '../home/home';
+import { TabsPage } from '../tabs/tabs';
+import { SignInPage } from '../sign-in/sign-in';
+import { GooglePlus } from "@ionic-native/google-plus";
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
-/**
- * Generated class for the StartPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
   selector: 'page-start',
   templateUrl: 'start.html',
 })
-export class StartPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public modalCtrl: ModalController) {
-  }
+export class StartPage {
+  userData: { email: any; name: any; uId: any; type: number; };
+
+  constructor(private googlePlus: GooglePlus, private fb: Facebook, private platform: Platform, public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StartPage');
   }
 
-  goRegister(){
+  goRegister() {
     this.navCtrl.push(RegisterPage)
   }
 
-  goSignIn(){
-    let myModal = this.modalCtrl.create(SignInPage,{
+  SignInStandBy(provider) {
+    console.log(provider)
+    this.platform.ready().then(x => {
+      alert("ready")
+      if (provider == "GOOGLE") {
+        alert("GOOGLE")
+        //this.loginGoogle({ email: "syafiq11a@gmail.com", name: "Syafiq", type: 2, uId: "1911575182192242" })
+
+        this.SignInGoogle()
+      }
+      else if (provider == "FACEBOOK") {
+        alert("FACEBOOK")
+        //this.loginFB({ email: "syafiq11a@gmail.com", name: "Syafiq", type: 3, uId: "1911575182192242" })
+
+        this.SignInFacebook()
+      }
+    })
+
+  }
+
+  SignInGoogle() {
+    this.googlePlus.login({}).then(y => {
+      this.userData = { email: y.email, name: y.displayName, uId: y.userId, type: 2 }
+      console.log(y)
+      this.loginGoogle(this.userData)
+      this.navCtrl.setRoot(TabsPage)
+
+    }).catch((c) => {
+      console.log(c)
+      alert("error")
+    })
+  }
+
+
+  loginGoogle(form) {
+    // this.loginService.postLoginGoogle(form).subscribe(x => {
+    //   if (x != null) {
+    //     console.log("loginGoogle Success", x)
+    //     this.navCtrl.setRoot(HomePage)
+    //   }
+    //   else {
+    //     alert("error")
+    //   }
+    // })
+  }
+
+
+  SignInFacebook() {
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+      .then((res: FacebookLoginResponse) => {
+        console.log('Logged into Facebook!', res.authResponse)
+        alert("success")
+        if (res.status == "connected") {
+          this.fb.api('me?fields=id,email,first_name', []).then(profile => {
+            this.userData = { email: profile['email'], name: profile['first_name'], uId: res.authResponse.userID, type: 3 }
+            console.log(this.userData)
+            this.loginFB(this.userData)
+            this.navCtrl.setRoot(TabsPage)
+
+          })
+        }
+        else {
+          alert("error login FACEBOOK")
+        }
+      })
+      .catch(e => console.log('Error logging into Facebook', e));
+  }
+
+  loginFB(form) {
+    // this.loginService.postLoginFacebook(form).subscribe(x => {
+    //   if (x != null) {
+    //     console.log("loginFb Success", x)
+    //     this.navCtrl.setRoot(HomePage)
+    //   }
+    //   else {
+    //     alert("error")
+    //   }
+    // })
+  }
+
+
+  goSignIn() {
+    let myModal = this.modalCtrl.create(SignInPage, {
       planCase: "userName"
     });
     myModal.present();
