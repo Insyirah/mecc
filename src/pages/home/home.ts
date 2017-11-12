21c0d1fcc8019cfdc3c6234ee2b91e1c05e3dcda
syafiq <syafiqabdrh@gmail.com>
2017-11-12 17:09:40 +0800

    ee

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {AboutPage} from '../about/about';
+import { AboutPage } from '../about/about';
 import { ListprovidersPage } from '../listproviders/listproviders';
 import { ServiceApiProvider } from '../../providers/service-api/service-api';
-
-
 
 @Component({
   selector: 'page-home',
@@ -17,75 +15,82 @@ export class HomePage {
   providerId: any;
   form: {};
   treatmentProvidedDetailID: number;
-  avatars : any[];
-  ava:any[];
-  
-constructor(private serviceApi : ServiceApiProvider,public navCtrl : NavController) {
+  avatars: any[];
+  ava: any[];
 
-this.avatars = [
-  {category:'1',img:"assets/dino.jpg"},
-  {category:'1',img:"assets/dino.jpg"},
-  {category:'1',img:"assets/dino.jpg"},
-  {category:'1',img:"assets/dino.jpg"},
-  {category:'1',img:"assets/dino.jpg"},
-  {category:'1',img:"assets/dino.jpg"},
-];
+  constructor(private serviceApi: ServiceApiProvider, public navCtrl: NavController) {
+   
+    this.avatars = [
+      { category: '1', img: "assets/dino.jpg" },
+      { category: '1', img: "assets/dino.jpg" },
+      { category: '1', img: "assets/dino.jpg" },
+      { category: '1', img: "assets/dino.jpg" },
+      { category: '1', img: "assets/dino.jpg" },
+      { category: '1', img: "assets/dino.jpg" },
+    ];
 
-}
-
-ionViewDidLoad(){
-  this.getFaceTreatment()
-  this.getHairTreatment()
-  this.getBodyTreatment()
-}
-
-getFaceTreatment(){
-  this.form={
-    moduleName : "UserApplication",
-    masterName : "List Of Face Treatment"
   }
-  this.serviceApi.getFaceTreatment(this.form).subscribe(data => {
-   this.faceTreatment = data
-   console.log("faceTreatment",this.faceTreatment) 
-  }) 
-}
 
-getHairTreatment(){
-  this.form={
-    moduleName : "UserApplication",
-    masterName : "List Of Hair Treatment"
+  ionViewDidLoad() {
+    this.getFaceTreatment()
+    this.getHairTreatment()
+    this.getBodyTreatment()
   }
-  this.serviceApi.getHairTreatment(this.form).subscribe(data => {
-   this.hairTreatment = data.masterData
-   console.log("hairTreatment",this.hairTreatment) 
-  }) 
-}
 
-goHairTreatmentProvider(MasterDataMaintenanceItemID){
-  this.navCtrl.push(ListprovidersPage,{
-    treatmentHairId:MasterDataMaintenanceItemID
-  })
-}
-
-getBodyTreatment(){
-  this.form={
-    moduleName : "UserApplication",
-    masterName : "List Of Body Treatment"
+  getFaceTreatment() {
+    this.form = {
+      moduleName: "UserApplication",
+      masterName: "List Of Face Treatment"
+    }
+    this.serviceApi.getFaceTreatment(this.form).subscribe(data => {
+      this.faceTreatment = data
+      console.log("faceTreatment", this.faceTreatment)
+    })
   }
-  this.serviceApi.getBodyTreatment(this.form).subscribe(data => {
-   this.bodyTreatment = data
-   console.log("bodyTreatment",this.bodyTreatment) 
-  }) 
-}
+
+  getHairTreatment() {
+    this.form = {
+      moduleName: "UserApplication",
+      masterName: "List Of Hair Treatment"
+    }
+    this.serviceApi.getHairTreatment(this.form).subscribe(data => {
+      this.hairTreatment = data.masterData
+      console.log("hairTreatment", this.hairTreatment)
+    })
+  }
+
+  // goHairTreatmentProvider(MasterDataMaintenanceItemID) {
+  //   this.navCtrl.push(ListprovidersPage, {
+  //     treatmentHairId: MasterDataMaintenanceItemID
+  //   })
+  // }
+
+  goTreatmentProvider(MasterDataMaintenanceItemID) {
+    this.navCtrl.push(ListprovidersPage, {
+      treatmentId: MasterDataMaintenanceItemID
+    })
+
+  }
+
+  getBodyTreatment() {
+    this.form = {
+      moduleName: "UserApplication",
+      masterName: "List Of Body Treatment"
+    }
+    this.serviceApi.getBodyTreatment(this.form).subscribe(data => {
+      this.bodyTreatment = data
+      console.log("bodyTreatment", this.bodyTreatment)
+    })
+  }
 
 
-test(){
-  
-  this.providerId=9
-  this.navCtrl.push(ListprovidersPage,{
-    providerID:this.providerId
-  });
-}
+  test() {
+
+    this.providerId = 9
+    this.navCtrl.push(ListprovidersPage, {
+      providerID: this.providerId
+    });
+  }
 
 }
 
